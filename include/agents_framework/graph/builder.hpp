791938bb375646253ee8398afd7c7d8a4a8c625a
Kaibo Huang <99372604+Kaibo-Huang@users.noreply.github.com>
2026-07-15 08:35:24 -0400

    graph: add conditional edges to the typed builder

--- include/agents_framework/graph/builder.hpp
+++ include/agents_framework/graph/builder.hpp
@@ -1,11 +1,14 @@
 #pragma once
 
+#include <concepts>
 #include <cstddef>
 #include <memory>
 #include <span>
 #include <string>
 #include <string_view>
+#include <type_traits>
 #include <utility>
+#include <vector>
 
 #include "agents_framework/core/result.hpp"
 #include "agents_framework/graph/graph.hpp"
@@ -13,6 +16,14 @@
 #include "agents_framework/graph/state.hpp"
 
 namespace agents_framework::graph {
+
+template <class F, class S>
+concept RouterFn =
+    std::invocable<F&, StateView<S>> &&
+    (std::same_as<std::invoke_result_t<F&, StateView<S>>, std::string> ||
+     std::same_as<std::invoke_result_t<F&, StateView<S>>, std::vector<std::string>> ||
+     std::same_as<std::invoke_result_t<F&, StateView<S>>, core::Result<std::string>> ||
+     std::same_as<std::invoke_result_t<F&, StateView<S>>, core::Result<std::vector<std::string>>>);
 
 template <class S>
 class GraphBuilder {
@@ -40,6 +51,29 @@ class GraphBuilder {
 
   GraphBuilder& set_finish(std::string node) {
     return add_edge(std::move(node), std::string{kEnd});
+  }
+
+  template <RouterFn<S> F>
+  GraphBuilder& add_conditional_edge(std::string from, F router,
+                                     std::vector<std::string> targets = {}) {
+    spec_.add_router(
+        std::move(from),
+        [router = std::move(router)](
+            const ChannelMap& state) mutable -> core::Result<std::vector<std::string>> {
+          using R = std::invoke_result_t<F&, StateView<S>>;
+          if constexpr (std::same_as<R, std::string>) {
+            return std::vector<std::string>{router(StateView<S>(state))};
+          } else if constexpr (std::same_as<R, std::vector<std::string>>) {
+            return router(StateView<S>(state));
+          } else if constexpr (std::same_as<R, core::Result<std::string>>) {
+            AF_TRY(auto name, router(StateView<S>(state)));
+            return std::vector<std::string>{std::move(name)};
+          } else {
+            return router(StateView<S>(state));
+          }
+        },
+        std::move(targets));
+    return *this;
   }
 
   [[nodiscard]] core::Result<CompiledGraph> compile() && { return std::move(spec_).compile(); }
