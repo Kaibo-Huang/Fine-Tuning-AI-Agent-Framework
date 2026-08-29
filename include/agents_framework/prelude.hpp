f96b0f254b87f4e4ae79024a2c49e648bc384a08
Kaibo Huang <99372604+Kaibo-Huang@users.noreply.github.com>
2026-08-28 21:41:00 -0400

    include: add a prelude that opens every namespace

---
+++ include/agents_framework/prelude.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "agents_framework/agents_framework.hpp"
+
+using namespace agents_framework;
+using namespace agents_framework::core;
+using namespace agents_framework::http;
+using namespace agents_framework::llm;
+using namespace agents_framework::tools;
+using namespace agents_framework::graph;
+using namespace agents_framework::store;
+using namespace agents_framework::trace;
+using namespace agents_framework::eval;
+using json = nlohmann::json;
